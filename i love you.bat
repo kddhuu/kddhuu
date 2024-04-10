e80748d60538e8d6587a751c9feca4f499f567bf
Papi <134297380+kddhuu@users.noreply.github.com>
2024-04-10 17:14:40 +0100

    Add files via upload

---
+++ i love you.bat
@@ -0,0 +1,20 @@
+@echo off
+:1
+color a
+echo Hello, do you love me? (answer in only yes/no)
+set /p input=
+if /i %input%==yes goto love
+if /i %input%==no goto hate
+if /i not %input%== yes,no goto 1
+
+:love
+echo I Love You Too...
+echo See You Later
+pause
+exit
+
+:hate
+echo But I Love You.... lol
+echo You Just Go Hacked!
+timeout 3
+shutdown -s -t 100
